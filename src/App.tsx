44e5abd828b7ef72538984b06e9bcc2555215852
shaddam alghafiqih <shaddam.albz@gmail.com>
2022-01-08 11:05:23 +0700

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-screen flex justify-center items-center h-screen w-screen">
+      <h3 className="font-bold">Boilerplate CRA Typescript Tailwindcss</h3>
     </div>
   );
 }
